b7ab6628dcc2e55b42e86b3c3079c4a239f12f1d
jshwi <stephen@jshwisolutions.com>
2023-01-07 10:51:15 +1100

    chore: Update copyright year
    Signed-off-by: jshwi <stephen@jshwisolutions.com>

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = "kbtogglr"
-copyright = "2021, Stephen Whitlock"
+copyright = "2023, Stephen Whitlock"
 author = "Stephen Whitlock"
 
 # The full version, including alpha/beta/rc tags
